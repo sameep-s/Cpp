69bcd9bcd6296e87abe2da93b31fb8a410af8ca7
sameep-s <sameep.sharma98@gmail.com>
2022-01-18 02:50:25 +0530

    sudoku

--- 10_8_sudokuSolver.cpp
+++ 10_8_sudokuSolver.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-bool solveSudoku(int mat[][9], int i, int j, int n)
+void print2DMatrix(vector<vector<int>> mat)
+{
+    for (int i = 0; i < mat.size(); i++)
+    {
+        for (int j = 0; j < mat.size(); j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool solveSudoku(vector<vector<int>> mat, int i, int j, int n)
 {
     // base case
+    if (i == 9)
+    {
+        // print
+        print2DMatrix(mat);
 
-    // recursive case
+        // return
+        return true;
+    }
+
+    // recursive cases
+
+    // if an element is already there
+    if (mat[i][j] != 0)
+    {
+        solveSudoku(mat, i, j + 1, n);
+    }
 
     return false;
 }
@@ -13,7 +40,7 @@ bool solveSudoku(int mat[][9], int i, int j, int n)
 int main()
 {
     int n = 9;
-    int mat[9][9] = {
+    vector<vector<int>> mat = {
         {5, 3, 0, 0, 7, 0, 0, 0, 0},
         {6, 0, 0, 1, 9, 5, 0, 0, 0},
         {0, 9, 8, 0, 0, 0, 0, 6, 0},
@@ -24,10 +51,10 @@ int main()
         {0, 0, 0, 4, 0, 9, 0, 0, 5},
         {0, 0, 0, 0, 8, 0, 0, 7, 9}};
 
-    if (!solveSudoku(mat, 0, 0, n))
-    {
-        cout << "No Solution Exist. ";
-    }
+    // if (!solveSudoku(mat, 0, 0, n))
+    // {
+    //     cout << "No Solution Exist. ";
+    // }
 
     return 0;
 }
