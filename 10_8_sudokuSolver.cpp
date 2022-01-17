d6cfa399a99b18d387a8d51d09ae6f7f9528fcc3
sameep-s <sameep.sharma98@gmail.com>
2022-01-18 04:02:37 +0530

    sudokuComplete

--- 10_8_sudokuSolver.cpp
+++ 10_8_sudokuSolver.cpp
@@ -2,6 +2,38 @@
 #include <vector>
 using namespace std;
 
+//-------------------- helperFucntion
+bool isGood(vector<vector<int>> mat, int i, int j, int n, int num)
+{
+
+    // check up, down, left, right
+    for (int x = 0; x < n; x++)
+    {
+        if (mat[i][x] == num || mat[x][j] == num)
+        {
+            return false;
+        }
+    }
+
+    // chech subGrid (formula (i/ underRoot(n))*underRoot(n))
+    int sx = (i / 3) * 3;
+    int sy = (j / 3) * 3;
+
+    for (int x = sx; x < (sx + 3); x++)
+    {
+        for (int y = sy; y < (sy + 3); y++)
+        {
+            if (mat[x][y] == num)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+//------------  To print Matrix
 void print2DMatrix(vector<vector<int>> mat)
 {
     for (int i = 0; i < mat.size(); i++)
@@ -14,26 +46,49 @@ void print2DMatrix(vector<vector<int>> mat)
     }
 }
 
-bool solveSudoku(vector<vector<int>> mat, int i, int j, int n)
+// main function sudokuSolver
+bool solveSudoku(vector<vector<int>> &mat, int i, int j, int n)
 {
     // base case
-    if (i == 9)
+    if (i == n)
     {
         // print
         print2DMatrix(mat);
+        cout << endl;
 
         // return
         return true;
     }
 
     // recursive cases
-
     // if an element is already there.
     if (mat[i][j] != 0)
     {
-        solveSudoku(mat, i, j + 1, n);
+        return solveSudoku(mat, i, j + 1, n);
     }
 
+    // if it's an end of the row
+    if (j == n)
+    {
+        return solveSudoku(mat, i + 1, 0, n);
+    }
+
+    // cases to be filled
+    for (int no = 1; no <= n; no++)
+    {
+        if (isGood(mat, i, j, n, no))
+        {
+            mat[i][j] = no;
+            bool solveSubProb = solveSudoku(mat, i, j + 1, n);
+            if (solveSubProb == true)
+            {
+                return true;
+            }
+        }
+    }
+
+    // if no option works (backtracking)
+    mat[i][j] = 0;
     return false;
 }
 
@@ -51,10 +106,10 @@ int main()
         {0, 0, 0, 4, 0, 9, 0, 0, 5},
         {0, 0, 0, 0, 8, 0, 0, 7, 9}};
 
-    // if (!solveSudoku(mat, 0, 0, n))
-    // {
-    //     cout << "No Solution Exist. ";
-    // }
+    if (!solveSudoku(mat, 0, 0, n))
+    {
+        cout << "No Solution Exist. ";
+    }
 
     return 0;
 }
